77c429f0bec14ce7fb6d45e8a9017ef00941b922
Yiming Chen <dsdshcym@gmail.com>
2015-10-14 10:18:47 +0800

    Get second partition table entry as pte_2

--- boot/arch/armv7a-le/zynq7000/bootmain.c
+++ boot/arch/armv7a-le/zynq7000/bootmain.c
@@ -8,17 +8,30 @@
  *
  */
 
+#define ELF32
+
 #include <config.h>
+#include <elf.h>
 
 #define PRELOAD_VECTOR_BASE	0x1ff00000
 
 void (*uart_init)(void) = (void *)(PRELOAD_VECTOR_BASE + 0x4);
 void (*uart_enable)(void) = (void *)(PRELOAD_VECTOR_BASE + 0x8);
 void (*uart_spin_puts)(const char *) = (void *)(PRELOAD_VECTOR_BASE + 0xC);
+void (*puthex)(u32 num) = (void *)(PRELOAD_VECTOR_BASE + 0x10);
+int (*sd_dma_spin_read)(u32 pa, u16 count, u32 offset) = (void *)(PRELOAD_VECTOR_BASE + 0x14);
 
-void mbr_bootmain(void)
-{
-	uart_spin_puts("Hello, AIMv6!\r\n");
-	while(1);
+void mbr_bootmain(void) {
+    /* mbr was already read in firmware_bootmain */
+    volatile u8 *mbr = (void *)0x100000;
+
+    u16 i;
+    u32 pte_2 = 0;
+    for (i = 11; i > 7; i--) {
+        pte_2 <<= 8;
+        pte_2 += mbr[462 + i];
+    }
+    volatile u8 *pte_2_p = (void *)pte_2;
+    while(1);
 }
 
