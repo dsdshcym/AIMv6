10ef90240d14301ff293a022153199f33e718a57
Yiming Chen <dsdshcym@gmail.com>
2015-10-25 09:11:04 +0800

    Add make scripts for kernel and output hello world

---
+++ kern/kernel.c
@@ -0,0 +1,5 @@
+#include "kernel.h"
+
+int main() {
+    uart_spin_puts("Hello World!\r\n");
+}
---
+++ kern/kernel.h
@@ -0,0 +1,9 @@
+#ifndef _KERNEL_H
+#define _KERNEL_H
+
+#include <config.h>
+#include <drivers/serial/uart.h>
+#include <drivers/sd/sd-zynq7000.h>
+#include <asm/io.h>
+
+#endif
