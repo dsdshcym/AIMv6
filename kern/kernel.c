f881533784e41d9078578dd3121977e556404dcc
Yiming Chen <dsdshcym@gmail.com>
2015-10-26 08:22:02 +0800

    Add entry for mmu

--- kern/kernel.c
+++ kern/kernel.c
@@ -1,5 +1,10 @@
 #include "kernel.h"
+#include "mmu.h"
 
 int main() {
-    uart_spin_puts("Hello World!\r\n");
+    uart_spin_puts("Entering Kernel!\r\n");
+
+    uart_spin_puts("Enabling MMU.\r\n");
+    enable_MMU();
+    uart_spin_puts("MMU Enabled.\r\n");
 }
